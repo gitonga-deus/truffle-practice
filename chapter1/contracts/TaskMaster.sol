1af46af5a7c23fb225f6be572def2129122b5149
gitonga <gitonga.deus@gmail.com>
2022-12-20 02:18:31 +0300

    wrapping it up

--- chapter1/contracts/TaskMaster.sol
+++ chapter1/contracts/TaskMaster.sol
@@ -1,9 +1,37 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.17 <0.7.0;
 
 contract TaskMaster {
-    mapping(address => uint) public balances; // balances of everyone
+    // balances of everyone
+    mapping(address => uint) public balances;
 
-    function TaskMaster() public {
+    // owner of the contract
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
         balances[msg.sender] = 10000;
+    }
+
+    function reward(
+        address doer,
+        uint rewardAmount
+    ) public returns (bool sufficientFunds) {
+        balances[msg.sender] -= rewardAmount;
+        balances[doer] += rewardAmount;
+        return sufficientFunds;
+    }
+
+    function getBalance(address addr) public view returns (uint) {
+        return balances[addr];
+    }
+
+    modifier isOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier hasSufficientFunds(uint rewardAmount) {
+        require(balances[msg.sender] >= rewardAmount);
+        _;
     }
 }
--- chapter1/migrations/2_deploy_contracts.js
+++ chapter1/migrations/2_deploy_contracts.js
@@ -1,8 +1,5 @@
-const ConvertLib = artifacts.require("ConvertLib");
-const MetaCoin = artifacts.require("MetaCoin");
+var TaskMaster = artifacts.require("./TaskMaster.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(ConvertLib);
-  deployer.link(ConvertLib, MetaCoin);
-  deployer.deploy(MetaCoin);
+export default function (deployer) {
+	deployer.deploy(TaskMaster);
 };

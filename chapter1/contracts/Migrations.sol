e1d65ec4e83c7eed58572fdda6c3d3a686b816d3
gitonga <gitonga.deus@gmail.com>
2022-12-20 01:55:57 +0300

    housekeeping

--- chapter1/contracts/Migrations.sol
+++ chapter1/contracts/Migrations.sol
@@ -1,19 +1,23 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.4.17;
 
 contract Migrations {
     address public owner;
-    uint public last_completed_migration;
-
-    constructor() public {
-        owner = msg.sender;
-    }
+    uint public lastCompletedMigration;
 
     modifier restricted() {
         if (msg.sender == owner) _;
     }
 
+    function Migrations() public {
+        owner = msg.sender;
+    }
+
     function setCompleted(uint completed) public restricted {
-        last_completed_migration = completed;
+        lastCompletedMigration = completed;
+    }
+
+    function upgrade(address newAddress) public restricted {
+        Migrations upgraded = Migrations(newAddress);
+        upgraded.setCompleted(lastCompletedMigration);
     }
 }
